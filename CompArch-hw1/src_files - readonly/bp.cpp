32d18cad95a9cd790ad1d885325ce2626fd54823
liorkest@gmail.com <liorkest@gmail.com>
2024-02-13 16:46:59 +0200

    add class definition

--- CompArch-hw1/src_files - readonly/bp.cpp
+++ CompArch-hw1/src_files - readonly/bp.cpp
@@ -3,8 +3,35 @@
 
 #include "bp_api.h" 
 
+class branch_predictor {
+	private:
+		btb_record *BTB_table; // size = btbSize
+		unsigned *history; // size = historySize
+		short **bimodal_counters; 
+	public:
+		branch_predictor()	{}
+		int init(unsigned btbSize, unsigned historySize, unsigned tagSize, unsigned fsmState,
+			bool isGlobalHist, bool isGlobalTable, int Shared);
+};
+
+int branch_predictor::init(unsigned btbSize, unsigned historySize, unsigned tagSize, unsigned fsmState,
+			bool isGlobalHist, bool isGlobalTable, int Shared)
+{
+
+}
+
+struct btb_record{
+	int tag;
+	uint32_t dst_addr;
+	int * history;
+};
+
+
+branch_predictor bp;
+
 int BP_init(unsigned btbSize, unsigned historySize, unsigned tagSize, unsigned fsmState,
 			bool isGlobalHist, bool isGlobalTable, int Shared){
+	bp.init();
 	return -1;
 }
 
@@ -19,4 +46,5 @@ void BP_update(uint32_t pc, uint32_t targetPc, bool taken, uint32_t pred_dst){
 void BP_GetStats(SIM_stats *curStats){
 	return;
 }
+
 
